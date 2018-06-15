029c40836d865bbc9e25302059f76aec9bf1cea5
lucygoldberg <lucygoldberg88@gmail.com>
2018-06-16 01:23:12 +0300

    add CreditCardDetector

---
+++ src/main/java/com/proofpoint/dlp/service/CreditCardDetector.java
@@ -0,0 +1,48 @@
+package com.proofpoint.dlp.service;
+
+import org.apache.commons.validator.routines.CreditCardValidator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CreditCardDetector implements IDetector{
+    private DetectorType type;
+    private Pattern pattern;
+    private String contextPattern;
+    private CreditCardValidator creditCardValidator;
+    @Override
+    public boolean detect(String text) {
+        if (!text.matches(contextPattern)) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            if (matcher.groupCount() >= 1) {
+                String code = matcher.group(1);
+                return creditCardValidator.isValid(code.replaceAll("[\\s|-]+", ""));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public DetectorType getType() {
+        return type;
+    }
+
+    public void setType(DetectorType type) {
+        this.type = type;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = Pattern.compile(pattern);
+    }
+
+    public void setContextPattern(String contextPattern) {
+        this.contextPattern = contextPattern;
+    }
+
+    public void setCreditCardValidator(CreditCardValidator creditCardValidator) {
+        this.creditCardValidator = creditCardValidator;
+    }
+}
