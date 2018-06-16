8728ae074dd725318153cdaab679a04de29d79bf
lucygoldberg <lucygoldberg88@gmail.com>
2018-06-16 10:27:53 +0300

    change return type

--- src/main/java/com/proofpoint/dlp/service/DetectorService.java
+++ src/main/java/com/proofpoint/dlp/service/DetectorService.java
@@ -2,14 +2,22 @@ package com.proofpoint.dlp.service;
 
 import com.proofpoint.dlp.detector.IDetector;
 import com.proofpoint.dlp.entity.DetectorType;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DetectorService {
     private List<IDetector> detectors;
-    public List<DetectorType> getSensitiveTypes(String text) {
-        return detectors.stream().filter(detector -> detector.detect(text)).map(IDetector::getType).collect(Collectors.toList());
+    public String getSensitiveTypes(String text) {
+        if (StringUtils.isEmpty(text)) {
+            return "";
+        }
+        return detectors.stream()
+                .filter(detector -> detector.detect(text))
+                .map(IDetector::getType)
+                .map(DetectorType::toString)
+                .collect(Collectors.joining(","));
     }
 
     public void setDetectors(List<IDetector> detectors) {
